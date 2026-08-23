f31eca794c13b8827de41bbf894f3f555da7e83f
jose_alberto <149262897+TeaTror99@users.noreply.github.com>
2026-08-23 14:54:28 +0000

    Complete the first five exercises from foundations

--- foundations/05_joinStrings/joinStrings.js
+++ foundations/05_joinStrings/joinStrings.js
@@ -9,6 +9,26 @@
 
 // Add your code right below, good luck!
 
+const firstName = "Carlos";
+const lastName = "Stevenson"
+let thisYear = 1965;
+const birthYear = 1947;
+const fullName = firstName + " " + lastName;
+let age = thisYear - birthYear;
+
+let greeting = "Hello! My name is " +  fullName + " " + "and I am " + age + " " +  "years old.";
+
+
+
+
+module.exports =  {
+  firstName,
+  lastName,
+  thisYear,
+  birthYear,
+  greeting
+}
+
 
 
 
